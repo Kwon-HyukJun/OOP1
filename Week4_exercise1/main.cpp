f05cbf537fdc993da6d8c7cb1319e94625a2cdcb
Kwon-HyukJun <ableman06@yonsei.ac.kr>
2025-09-25 17:48:51 +0900

    commit1

---
+++ Week4_exercise1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello world!" << std::endl;
+    std::cout << "Kwon Hyukjun" << std::endl;
+    std::cout << 3 + 2 << std::endl;
+    return 0;
+}
