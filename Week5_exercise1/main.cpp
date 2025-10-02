7b303b73998ba569f8a817d4122fa544f2b2b25e
Kwon-HyukJun <ableman06@yonsei.ac.kr>
2025-10-02 17:17:19 +0900

    fin

--- Week5_exercise1/main.cpp
+++ Week5_exercise1/main.cpp
@@ -14,20 +14,9 @@ int main()
     for(int i = 2; i >= 0; i--)
         reversed_b += num_b[i];
 
-    bool flag = 0;
-
-    if(reversed_a[0] < reversed_b[0])
-        flag = 1;
-    else if(reversed_a[0] == reversed_b[0]){
-        if (reversed_a[1] < reversed_b[1])
-            flag = 1;
-        else if(reversed_a[1] == reversed_b[1] && reversed_a[2] < reversed_b[2])
-            flag = 1;
-    }
-
-    if(flag == 0)
+    if(reversed_a > reversed_b)
         std::cout << reversed_a;
-    else if(flag == 1)
+    else
         std::cout << reversed_b;
 
     return 0;
